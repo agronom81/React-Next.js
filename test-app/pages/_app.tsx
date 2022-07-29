663b06e3347bb145000444ff8b57e5bd99aae790
Pavlo Petrenko <agronom81@gmail.com>
2022-07-29 08:17:27 +0300

    create components folder, add htag component

---
+++ test-app/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) : JSX.Element {
+  return <>
+    <Head>
+        <title>Next App</title>
+        <link key={1} rel="icon" href="/favicon.ico2" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+    <Component {...pageProps} />
+  </>;
+}
+
+export default MyApp;
---
+++ test-app/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Htag } from '../components';
+
+export default function Home(): JSX.Element {
+  return (
+   <div>
+    <Htag tag='h1'>Text</Htag>
+   </div> 
+  );
+}
